ee51ec95bf5d99f56649be96a6619d8028cd7423
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-01-06 23:43:30 +0100

    fixed typo

--- src/components/weatherCard/WeatherForm.js
+++ src/components/weatherCard/WeatherForm.js
@@ -5,25 +5,30 @@ import { useContext } from 'react'
 import { WeatherContext } from '../../store/weather-context'
 
 const WeatherForm = (props) => {
-    let city;
+    const [isLoading, setIsLoading] = useState('false')
+
+
     const locationInputRef = useRef()
     const weatherCtx = useContext(WeatherContext)
     const api_key = '5fc53b4163498db65d6329e64f584aea'
 
 
     const onSubmitHandler =  (e) =>{
-        city = locationInputRef.current.value
+        setIsLoading(true)
         e.preventDefault()
-        const fetchCity = async (city) => {
-            const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},CA&appid=${api_key}`)
+        const fetchCity = async () => {
+            const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationInputRef.current.value.trim()}&appid=${api_key}`)
             const data = await response.json()
             return data
             
           }
-          fetchCity(city).then(res => {
-              console.log(res.weather[0].icon)
+          fetchCity().then(res => {
+              console.log(res)
               weatherCtx.setCity(res)
+              
           })
+
+          setIsLoading(false)
           
     }   
 
@@ -31,7 +36,7 @@ const WeatherForm = (props) => {
         <React.Fragment>
             <form onSubmit={onSubmitHandler}>
                 <input ref={locationInputRef} type="text" />
-                <button type='submit'>Get weather</button>
+                <button type='submit'>{isLoading ? 'is loading...' : 'Get weather'}</button>
             </form>
             
         </React.Fragment>
