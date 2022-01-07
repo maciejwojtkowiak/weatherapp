fd49b7a14b8e8da72b95f1ad5acc5f63b0c42acd
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-01-07 09:01:57 +0100

    created custom hook

---
+++ src/components/hooks/use-http.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { WeatherContext } from "../../store/weather-context"
+import { useContext } from "react"
+
+const useHttp = () => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+    const weatherCtx = useContext(WeatherContext)
+
+    const getCityData = (url) => {
+        setIsLoading(true)
+        const fetchCity = async () => {
+            const response  = await fetch(url)
+            if (!response.ok) {
+                throw new Error('City was not found.')
+            }
+            const data = await response.json()
+            return data
+            
+          }
+
+          try {
+            fetchCity().then(res => {
+                console.log(res)
+                weatherCtx.setCity(res)
+                setIsLoading(false)
+                
+            })
+          } catch (err) {
+              console.log(err)
+              setIsLoading(false)
+          }
+
+    }
+       
+          return {
+              isLoading,
+              error,
+              getCityData
+          }
+    }
+
+    export default useHttp
+       
--- src/components/weatherCard/WeatherForm.js
+++ src/components/weatherCard/WeatherForm.js
@@ -3,44 +3,22 @@ import React from 'react'
 import { useRef, useState } from 'react'
 import { useContext } from 'react'
 import { WeatherContext } from '../../store/weather-context'
+import useHttp from '../hooks/use-http'
 
 const WeatherForm = (props) => {
-    const [isLoading, setIsLoading] = useState(false)
-
-
+   
+   
     const locationInputRef = useRef()
-    const weatherCtx = useContext(WeatherContext)
     const api_key = '5fc53b4163498db65d6329e64f584aea'
-
-
+    const {isLoading, getCityData} = useHttp()
     const onSubmitHandler =  (e) =>{
-        setIsLoading(true)
         e.preventDefault()
-        const fetchCity = async () => {
-            const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationInputRef.current.value.trim()}&appid=${api_key}`)
-            if (!response.ok) {
-                throw new Error('City was not found.')
-            }
-            const data = await response.json()
-            return data
-            
-          }
-
-          try {
-            fetchCity().then(res => {
-                console.log(res)
-                weatherCtx.setCity(res)
-                setIsLoading(false)
-                
-            })
-          } catch (err) {
-              console.log(err)
-              setIsLoading(false)
-          }
+        getCityData(`https://api.openweathermap.org/data/2.5/weather?q=${locationInputRef.current.value.trim()}&units=metric&appid=${api_key}`)
     }   
 
     const onSubmitHandlerRandom = (e) => {
         e.preventDefault();
+       
 
         console.log(navigator.geolocation.getCurrentPosition((pos) => {
             const {latitude, longitude} = pos.coords
@@ -59,7 +37,7 @@ const WeatherForm = (props) => {
             </form>
             <form onSubmit={onSubmitHandlerRandom}>
                 <div className={styles.formContainer}>   
-                    <button type='submit'>{isLoading  ? 'is loading...' : 'Random localization'}</button>
+                    <button type='submit'>{isLoading  ? 'is loading...' : 'Get your location'}</button>
                 </div>
             </form>
             
